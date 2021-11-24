8370af78ecce0d7d664ce2c2a33cc20e4d00510e
shalabyelectronics <mhdshalabystar@gmail.com>
2021-11-24 23:02:03 +0300

    create a canvas and add a tomato image

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 from tkinter import *
+
 # First we are going to import all classes from tkinter module.
 
-#--------------------- Create application GUI --------------------#
+# --------------------- Create application GUI --------------------#
 window = Tk()
 window.title("Pomodoro")
 # Add padding to our window padx and pady.
 window.config(padx=100, pady=50)
-
-
-
-
-
+# create a pomodoro_img object from the class PhotoImage.
+pomodoro_img = PhotoImage(file="pomodoro.png")
+canvas = Canvas(width=500, height=500)
+canvas.create_image(250, 250, image=pomodoro_img)
+canvas.pack()
 
 window.mainloop()
