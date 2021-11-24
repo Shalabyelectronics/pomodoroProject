3b79c29ad6ad83e0d1a7e6d3651c4c2af33176fc
shalabyelectronics <mhdshalabystar@gmail.com>
2021-11-24 22:40:21 +0300

    Create a window from Tk class

---
+++ main.py
@@ -0,0 +1,15 @@
+from tkinter import *
+# First we are going to import all classes from tkinter module.
+
+#--------------------- Create application GUI --------------------#
+window = Tk()
+window.title("Pomodoro")
+# Add padding to our window padx and pady.
+window.config(padx=100, pady=50)
+
+
+
+
+
+
+window.mainloop()
